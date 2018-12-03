10d15fb0bcfebafbfc8af786f949b43fbb19b74a
scapegoat <andreygluhih99@gmail.com>
2018-12-03 18:50:46 +0300

    Initial Commit

---
+++ rssReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rssReader
+//
+//  Created by Андрей Глухих on 03/12/2018.
+//  Copyright © 2018 scapegoat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
